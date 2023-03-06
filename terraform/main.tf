e48e38b2171b4e5a02d9b3ba21996453d5e9b923
garynation <44471822+garynation@users.noreply.github.com>
2023-03-06 10:21:49 +0000

    updates3

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ terraform {
 
   required_version = ">=0.14.9"
 
+  backend "s3" {
+       bucket = "garynationtfstate"
+       key    = "[Remote_State_S3_Bucket_Key]"
+       region = "east-us-1"
+   }
+
 }
 
 provider "aws" {
